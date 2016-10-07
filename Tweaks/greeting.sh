5d28361fda4fdf3b43ec3cb303f806e2f5e467f9
toloveru <mdr1996@live.be>
2016-10-07 23:41:53 +0200

    Revert "Developing again!"
    This reverts commit 5080af0449de5c152f6f92eae12aefd58f0d5616.

--- Tweaks/greeting.sh
+++ Tweaks/greeting.sh
@@ -3,19 +3,18 @@ HOUR=$(date +%H)
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-
 if (( "$HOUR" < 6 && "$HOUR" >= 0 ))
 then
-	printf '\e[31mGood night!\e[m\n'
+	printf '\e[31;40mGood night!\e[m\n'
 elif (( "$HOUR" >= 6 && "$HOUR" < 12 ))
 then
-	printf '\e[31mGood morning!\e[m\n'
+	printf '\e[31;40mGood morning!\e[m\n'
 elif (( "$HOUR" >= 12 && "$HOUR" < 18 ))
 then
-	printf '\e[31mGood afternoon!\e[m\n'
+	printf '\e[31;40mGood afternoon!\e[m\n'
 elif (( "$HOUR" >= 18 && "$HOUR" < 24 ))
 then
-	printf '\e[31mGood evening!\e[m\n'
+	printf '\e[31;40mGood evening!\e[m\n'
 else
 	echo "Hi there!"
 fi
