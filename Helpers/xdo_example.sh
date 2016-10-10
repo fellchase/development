be062ad453399c45927b14ab41f5fb7f07ff757e
toloveru <mdr1996@live.be>
2016-10-10 11:15:56 +0200

    xdotool example

---
+++ Helpers/xdo_example.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This is only necessary when your keyboard layout is not QWERTY.
+setxkbmap be
+
+# Move mouse to specified location
+xdotool mousemove 939 280
+xdotool click 1
+
+# Start VIM
+xdotool type "vim"
+xdotool key Return
+xdotool type "i"
+
+# Start for loop.
+for num in {1..50}
+do
+	xdotool type "Tweet no. $num."
+	xdotool key Return
+done
+
+# Set VIM back to Normal Mode
+xdotool key Escape
